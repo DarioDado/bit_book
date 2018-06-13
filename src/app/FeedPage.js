267074a399be92327c8f385f7a34d08fa80bcc68
BIT Student <classroom@bgit.rs>
2018-06-13 17:59:58 +0200

    add postService, finsh getPosts method

--- src/app/FeedPage.js
+++ src/app/FeedPage.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './FeedPage.css';
 import {PostList} from './posts/PostList';
+import { postService } from '../services/postService';
 
 
 export class FeedPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            posts: null,
+            loading: true
+        }
+    }
+
+    componentDidMount = () => {
+        postService.getPosts()
+            .then(posts => {
+                console.log(posts);
+            })
+            .catch(e=> {
+                console.log(e);
+            })
     }
 
     render() {
