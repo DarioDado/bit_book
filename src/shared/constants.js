423d7196c581d2324b94b2ccf763713c2c056b39
BIT Student <classroom@bgit.rs>
2018-06-13 12:23:09 +0200

    [bbkt-5] set up server api constants

---
+++ src/shared/constants.js
@@ -0,0 +1,21 @@
+export const endpoints = {
+    login: "http://bitbookapi.azurewebsites.net/api/login",
+    register: "http://bitbookapi.azurewebsites.net/api/register",
+    comments: "http://bitbookapi.azurewebsites.net/api/Comments",
+    imagePosts: "http://bitbookapi.azurewebsites.net/api/ImagePosts",
+    postsCount: "http://bitbookapi.azurewebsites.net/api/posts/count",
+    posts: "http://bitbookapi.azurewebsites.net/api/Posts",
+    profile: "http://bitbookapi.azurewebsites.net/api/profile",
+    editProfileEndpoint: "http://bitbookapi.azurewebsites.net/api/Profiles",
+    users: "http://bitbookapi.azurewebsites.net/api/users",
+    users: "http://bitbookapi.azurewebsites.net/api/users",
+    textPosts: "http://bitbookapi.azurewebsites.net/api/TextPosts",
+    upload: "http://bitbookapi.azurewebsites.net/api/upload",
+    videoPosts: "http://bitbookapi.azurewebsites.net/api/VideoPosts",
+}
+
+export const requestHeader = {
+    "Content-Type": "application/json",
+    "Key": "bitbookdev",
+    "SessionId": "2990B489-DB94-4AC1-ACDE-CDC9CC3EAEAE"
+}
