2bb184f095b541e14fdf82427710ac25c483e943
BIT Student <classroom@bgit.rs>
2018-06-13 11:54:04 +0200

    [bbkt-4] set up materializeCSS library

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'materialize-css/dist/css/materialize.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
